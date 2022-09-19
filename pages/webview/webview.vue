0a7e80687a1cd1704bb97a82a703c4c021c6bed1
AriseMarcher <2543061999@qq.com>
2022-09-19 17:01:32 +0800

    首页轮播图基础设置

---
+++ pages/webview/webview.vue
@@ -0,0 +1,22 @@
+<template>
+	<web-view :src="url"></web-view>
+</template>
+
+<script>
+	export default {
+		props: {
+			url: {
+				type: String
+			}
+		},
+		onLoad(options) {
+			console.log(options)
+			this.url = decodeURI((options.url))
+			console.log(this.url)
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
